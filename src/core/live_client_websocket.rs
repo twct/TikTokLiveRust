90fd3eb166b1d1c7311b5851dfb9f52dd8ea97a6
Astro <764664@gmail.com>
2024-05-08 12:02:56 +0900

    Switch to tokio spawn

--- src/core/live_client_websocket.rs
+++ src/core/live_client_websocket.rs
@@ -1,8 +1,8 @@
-use std::ops::Deref;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
 use protobuf::Message;
+use std::ops::Deref;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
 use tokio::runtime::Runtime;
 use tokio_tungstenite::tungstenite::connect;
 use tokio_tungstenite::tungstenite::handshake::client::Request;
@@ -11,18 +11,15 @@ use crate::core::live_client::TikTokLiveClient;
 use crate::core::live_client_mapper::TikTokLiveMessageMapper;
 use crate::data::live_common::ConnectionState::CONNECTED;
 use crate::generated::events::{TikTokConnectedEvent, TikTokLiveEvent};
-use crate::http::http_data::LiveConnectionDataResponse;
 use crate::generated::messages::webcast::{WebcastPushFrame, WebcastResponse};
+use crate::http::http_data::LiveConnectionDataResponse;
 
-
-pub struct TikTokLiveWebsocketClient
-{
+pub struct TikTokLiveWebsocketClient {
     pub(crate) message_mapper: TikTokLiveMessageMapper,
     pub(crate) running: Arc<AtomicBool>,
 }
 
-impl TikTokLiveWebsocketClient
-{
+impl TikTokLiveWebsocketClient {
     pub fn new(message_mapper: TikTokLiveMessageMapper) -> Self {
         TikTokLiveWebsocketClient {
             message_mapper,
@@ -30,10 +27,11 @@ impl TikTokLiveWebsocketClient
         }
     }
 
-
-    pub async fn start(&self, response: LiveConnectionDataResponse,  client: Arc<TikTokLiveClient>)
-    {
-        let host = response.web_socket_url.host_str().expect("Invalid host in WebSocket URL");
+    pub async fn start(&self, response: LiveConnectionDataResponse, client: Arc<TikTokLiveClient>) {
+        let host = response
+            .web_socket_url
+            .host_str()
+            .expect("Invalid host in WebSocket URL");
 
         let request = Request::builder()
             .method("GET")
@@ -52,30 +50,29 @@ impl TikTokLiveWebsocketClient
         client.set_connection_state(CONNECTED);
         client.publish_event(TikTokLiveEvent::OnConnected(TikTokConnectedEvent {}));
 
-
         let running = self.running.clone();
         running.store(true, Ordering::SeqCst);
 
         let message_mapper = self.message_mapper.clone();
 
-        thread::spawn(move || {
-            while running.load(Ordering::SeqCst)
-            {
+        tokio::spawn(async move {
+            while running.load(Ordering::SeqCst) {
                 let optional_message = socket.read_message();
 
-                if (optional_message.is_err())
-                {
+                if (optional_message.is_err()) {
                     continue;
                 }
                 let message = optional_message.unwrap();
 
                 let buffer = message.into_data();
 
-                let mut push_frame = WebcastPushFrame::parse_from_bytes(buffer.as_slice()).expect("Unable to read push frame");
-                let webcast_response = WebcastResponse::parse_from_bytes(push_frame.Payload.as_mut_slice()).expect("Unable to read webcast response");
+                let mut push_frame = WebcastPushFrame::parse_from_bytes(buffer.as_slice())
+                    .expect("Unable to read push frame");
+                let webcast_response =
+                    WebcastResponse::parse_from_bytes(push_frame.Payload.as_mut_slice())
+                        .expect("Unable to read webcast response");
 
-                if webcast_response.needsAck
-                {
+                if webcast_response.needsAck {
                     let mut push_frame_ack = WebcastPushFrame::new();
                     push_frame_ack.PayloadType = "ack".to_string();
                     push_frame_ack.LogId = push_frame.LogId;
@@ -83,7 +80,9 @@ impl TikTokLiveWebsocketClient
 
                     let binary = push_frame_ack.write_to_bytes().unwrap();
                     let message = tungstenite::protocol::Message::binary(binary);
-                    socket.write_message(message).expect("Unable to send ack packet");
+                    socket
+                        .write_message(message)
+                        .expect("Unable to send ack packet");
                 }
 
                 message_mapper.handle_webcast_response(webcast_response, client.as_ref());
@@ -91,8 +90,7 @@ impl TikTokLiveWebsocketClient
         });
     }
 
-    pub fn stop(&self)
-    {
+    pub fn stop(&self) {
         self.running.store(false, Ordering::SeqCst);
     }
 }
